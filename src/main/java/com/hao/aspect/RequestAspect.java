4dcfcdcc9808a4398b3e97e527ae86d951d27a59
hhj <haohj94@163.com>
2018-08-27 17:22:36 +0800

    日志输出

---
+++ src/main/java/com/hao/aspect/RequestAspect.java
@@ -0,0 +1,47 @@
+package com.hao.aspect;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class RequestAspect {
+    private Logger logger = Logger.getLogger(RequestAspect.class);
+
+    @Pointcut("execution(public * com.hao.controller.*.*(..))")
+    public void log() {
+
+    }
+
+    @Before("log()")
+    public void doBefore(JoinPoint joinPoint) {
+        logger.info("方法执行前...");
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = sra.getRequest();
+        logger.info("url:" + request.getRequestURI());
+        logger.info("ip:" + request.getRemoteHost());
+        logger.info("method:" + request.getMethod());
+        logger.info("class_method:" + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+        for (Object args : joinPoint.getArgs()) {
+            logger.info("args:" + args.toString());
+        }
+    }
+
+    @After("log()")
+    public void doAfter(JoinPoint joinPoint) {
+        logger.info("方法执行后...");
+    }
+
+    @AfterReturning(returning = "result", pointcut = "log()")
+    public void doAfterReturning(Object result) {
+        logger.info("方法返回值：" + result);
+    }
+
+}
