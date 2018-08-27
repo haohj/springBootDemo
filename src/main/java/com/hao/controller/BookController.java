9bb2217dceb2a78d72d84b4d52275b231cd71825
hhj <haohj94@163.com>
2018-08-27 15:14:42 +0800

    图书根据条件动态查询

--- src/main/java/com/hao/controller/BookController.java
+++ src/main/java/com/hao/controller/BookController.java
@@ -2,11 +2,17 @@ package com.hao.controller;
 
 import com.hao.dao.BookDao;
 import com.hao.entity.Book;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 @Controller
 @RequestMapping("/book")
@@ -15,6 +21,11 @@ public class BookController {
     @Resource
     private BookDao bookDao;
 
+    /**
+     * 查询所有图书
+     *
+     * @return
+     */
     @RequestMapping("/list")
     public ModelAndView bookList() {
         ModelAndView mv = new ModelAndView();
@@ -23,12 +34,56 @@ public class BookController {
         return mv;
     }
 
+    /**
+     * 根据条件动态查询
+     *
+     * @param book
+     * @return
+     */
+    public ModelAndView bookList2(Book book) {
+        ModelAndView mv = new ModelAndView();
+
+        List<Book> bookList = bookDao.findAll(new Specification<Book>() {
+
+            @Override
+            public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                Predicate predicate = cb.conjunction();
+                if (book != null) {
+                    if (book.getBookName() != null && !"".equals(book.getBookName())) {
+                        predicate.getExpressions().add(cb.like(root.get("name"), "%" + book.getBookName() + "%"));
+                    }
+                    if (book.getAuthor() != null && !"".equals(book.getAuthor())) {
+                        predicate.getExpressions().add(cb.like(root.get("author"), "%" + book.getAuthor() + "%"));
+                    }
+                }
+                return predicate;
+            }
+        });
+
+        mv.addObject("bookList", bookList);
+        mv.setViewName("bookList");
+        return mv;
+    }
+
+
+    /**
+     * 添加图书
+     *
+     * @param book
+     * @return
+     */
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String add(Book book) {
         bookDao.save(book);
         return "forward:/book/list";
     }
 
+    /**
+     * 根据id查询book实体
+     *
+     * @param id
+     * @return
+     */
     @RequestMapping("/preUpdate/{id}")
     public ModelAndView preUpdate(@PathVariable(value = "id") Integer id) {
         ModelAndView mv = new ModelAndView();
@@ -45,18 +100,36 @@ public class BookController {
     }
 
 
+    /**
+     * 修改图书
+     *
+     * @param book
+     * @return
+     */
     @PostMapping("/update")
     public String update(Book book) {
         bookDao.save(book);
         return "forward:/book/list";
     }
 
+    /**
+     * 删除图书
+     *
+     * @param id
+     * @return
+     */
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable(value = "id") Integer id) {
         bookDao.deleteById(id);
         return "forward:/book/list";
     }
 
+    /**
+     * 根据图书名称模糊查询
+     *
+     * @param bookName
+     * @return
+     */
     @RequestMapping("/queryByBookName")
     public ModelAndView queryByBookName(String bookName) {
         ModelAndView mv = new ModelAndView();
@@ -65,6 +138,11 @@ public class BookController {
         return mv;
     }
 
+    /**
+     * 随机查询两条图书记录
+     *
+     * @return
+     */
     @GetMapping("/randomList")
     public ModelAndView randomList() {
         ModelAndView mv = new ModelAndView();
--- src/main/java/com/hao/dao/BookDao.java
+++ src/main/java/com/hao/dao/BookDao.java
@@ -2,6 +2,7 @@ package com.hao.dao;
 
 import com.hao.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -9,11 +10,12 @@ import java.util.List;
 /**
  * @author Administrator
  */
-public interface BookDao extends JpaRepository<Book, Integer> {
+public interface BookDao extends JpaRepository<Book, Integer> , JpaSpecificationExecutor<Book> {
 
     @Query("select b from t_book b where b.book_name like '%?1%'")
     public List<Book> findByName(String bookName);
 
     @Query(value = "select * from t_book order by rand() limit ?1", nativeQuery = true)
     public List<Book> randomList(Integer n);
+
 }
