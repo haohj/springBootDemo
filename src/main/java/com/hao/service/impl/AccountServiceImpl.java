e104f0dc922bd6dd6d66c31e2548ca9f623296c4
hhj <haohj94@163.com>
2018-08-27 17:34:25 +0800

    事务

--- src/main/java/com/hao/service/impl/AccountServiceImpl.java
+++ src/main/java/com/hao/service/impl/AccountServiceImpl.java
@@ -6,6 +6,7 @@ import com.hao.service.AccountService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 /**
  * 帐号Service实现类
@@ -18,17 +19,17 @@ public class AccountServiceImpl implements AccountService {
     private AccountDao accountDao;
 
     @Override
+    @Transactional
     public void transferAccounts(int fromUser, int toUser, float account) {
         //转账账户扣除转账金额
         Account fromAccount = accountDao.getOne(fromUser);
         fromAccount.setBalance(fromAccount.getBalance() - account);
+        accountDao.save(fromAccount);
 
         //转入账户增加转账金额
         Account toAccount = accountDao.getOne(toUser);
         toAccount.setBalance(toAccount.getBalance() + account);
 
-        //保存
-        accountDao.save(fromAccount);
         int zero = 1 / 0;
         accountDao.save(toAccount);
     }
